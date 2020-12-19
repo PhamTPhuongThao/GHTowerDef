61789b9de98f82b6b753c4e7ce55cc719bf48d45
phamtphuongthao <phamtphuongthao24@gmail.com>
2020-12-19 16:20:54 +0700

    update get configure of Hero

--- Assets/TowerDefense/Scripts/Core/HeroLoader.cs
+++ Assets/TowerDefense/Scripts/Core/HeroLoader.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO;
 using UnityEngine;
 
 public class HeroLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public TextAsset textJSON;
+
+    [System.Serializable]
+    public class Hero
     {
-        
+        public int hero;
+        public int attackType;
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class HeroesCollection
     {
-        
+        public int numberOfHero;
+        public Hero[] heroes;
+
+    }
+    public HeroesCollection heroesCollection = new HeroesCollection();
+
+    [ContextMenu("Load Heroes Configure")]
+    private void Start()
+    {
+        heroesCollection = JsonUtility.FromJson<HeroesCollection>(textJSON.text);
+    }
+    private void Update()
+    {
+
     }
 }
